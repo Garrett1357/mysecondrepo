527c4e20bc2d307413cd82666f273a4e5f93c265
Garrett <Garrett_austin2@baylor.edu>
2024-01-31 14:31:29 -0600

    FInished employee.py

--- employee_pay.py
+++ employee_pay.py
@@ -1,17 +1,16 @@
 import csv
 employee = open('employee_data.csv','r')
-#infile = open('customer_country.csv')
 csv_file = csv.reader(employee)
 #to skip the header and start on the next lone
 next(csv_file)
 for rec in csv_file:    #This brings each record into a list format
     #print (rec) 
-    salary = float(rec [3])
+    salary = int(rec [3])
     bonus = float(rec [7])
     total_bonus= (salary * bonus)
     total = salary + total_bonus
-    print("Name: ", {rec [2]})
-    print("Salary: ", {rec [4]})
-    print("Bonus: ", {total_bonus})
-    print("Total Salary: ", {total})#NAME SALARY BONUS PAY
+    print(f"Name:   {rec[1]}")
+    print(f'Salary: ${salary}')
+    print(f'Bonus:  ${total_bonus}')
+    print(f'Pay:    ${total}')
     input()
